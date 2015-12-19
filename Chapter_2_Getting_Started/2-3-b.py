9812edf4674ff5fd972891f54bfeb43c4a125afd
Zhao HG <cyberzhg@gmail.com>
2015-12-19 06:56:13 +0000

    Create Chapter_2_Getting_Started/2-3-b.py

---
+++ Chapter_2_Getting_Started/2-3-b.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+def polynomial_evaluation(a, x):
+    sum = 0
+    for i in range(len(a)):
+        sum += a[i] * x ** i
+    return sum
+
+
+class PolynomialEvaluationTestCase(unittest.TestCase):
+    def test_empty(self):
+        self.assertEqual(polynomial_evaluation([], 4), 0)
+
+    def test_const(self):
+        self.assertEqual(polynomial_evaluation([5], 2), 5)
+
+    def test_simple(self):
+        self.assertEqual(polynomial_evaluation([1, 2, 3], 4), 57)
+
+if __name__ == '__main__':
+    unittest.main()
