55634064df53d6e1eef84b0a78cf8ee4ecc7264b
Zhao HG <cyberzhg@gmail.com>
2015-12-31 08:49:01 +0000

    Create Chapter_9_Medians_and_Order_Statistics/9-2-b.py

---
+++ Chapter_9_Medians_and_Order_Statistics/9-2-b.py
@@ -0,0 +1,33 @@
+import random
+import unittest
+
+
+def weighted_median(x):
+    x.sort()
+    s = 0.0
+    for i in range(len(x)):
+        s += x[i]
+        if s >= 0.5:
+            return x[i]
+
+
+class ProblemTestCase(unittest.TestCase):
+
+    def test_random(self):
+        for _ in range(1000):
+            a = random.sample(range(10000), 1000)
+            s = sum(a)
+            x = [float(v) / s for v in a]
+            w = weighted_median(x)
+            s1, s2 = 0.0, 0.0
+            for v in x:
+                if v < w:
+                    s1 += v
+                elif v > w:
+                    s2 += v
+            self.assertTrue(s1 < 0.5)
+            self.assertTrue(s2 <= 0.5)
+
+
+if __name__ == '__main__':
+    unittest.main()
