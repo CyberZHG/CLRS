bde3d8c9b7a1046d7629eb42d7565815e221f8b2
Zhao HG <cyberzhg@gmail.com>
2015-12-31 05:54:09 +0000

    Create 9.3-5.py

---
+++ 9.3-5.py
@@ -0,0 +1,40 @@
+import random
+import unittest
+
+
+def black_box_median(a, p, r):
+    return sorted(a)[(p + r) // 2]
+
+
+def partition(a, p, r, x):
+    i = p - 1
+    for k in range(p, r):
+        if a[k] < x:
+            i += 1
+            a[i], a[k] = a[k], a[i]
+    return i
+
+
+def select(a, p, r, i):
+    if p + 1 == r:
+        return a[p]
+    x = black_box_median(a, p, r)
+    q = partition(a, p, r, x)
+    k = q - p + 1
+    if i <= k:
+        return select(a, p, q + 1, i)
+    return select(a, q + 1, r, i - k)
+
+
+class ProblemTestCase(unittest.TestCase):
+
+    def test_random(self):
+        for _ in range(1000):
+            a = [random.randint(1, 1000000) for _ in range(random.randint(1, 1000))]
+            i = random.randint(1, len(a))
+            b = sorted(a)
+            self.assertTrue(select(a, 0, len(a), i), b[i-1])
+
+
+if __name__ == '__main__':
+    unittest.main()
