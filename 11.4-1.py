e443737b34d91572a85fdd9ee966ca9106b50da3
Zhao HG <cyberzhg@gmail.com>
2016-01-03 14:15:16 +0000

    Create 11.4-1.py

---
+++ 11.4-1.py
@@ -0,0 +1,74 @@
+import random
+import unittest
+
+
+m = 11
+
+
+class LinearProbing:
+    def __init__(self):
+        global m
+        self.slots = [None for _ in xrange(m)]
+
+    def insert(self, key):
+        global m
+        i = 0
+        while True:
+            pos = (key + i) % m
+            if self.slots[pos] is None:
+                break
+            i += 1
+        self.slots[pos] = key
+
+
+class QuadraticProbing:
+    def __init__(self):
+        global m
+        self.slots = [None for _ in xrange(m)]
+
+    def insert(self, key):
+        global m
+        i = 0
+        while True:
+            pos = (key + i + 3 * i * i) % m
+            if self.slots[pos] is None:
+                break
+            i += 1
+        self.slots[pos] = key
+
+
+class DoubleHashing:
+    def __init__(self):
+        global m
+        self.slots = [None for _ in xrange(m)]
+
+    def insert(self, key):
+        global m
+        i = 0
+        h2 = 1 + (key % (m - 1))
+        while True:
+            pos = (key + i * h2) % m
+            if self.slots[pos] is None:
+                break
+            i += 1
+        self.slots[pos] = key
+
+
+class ProblemTestCase(unittest.TestCase):
+
+    def test_case(self):
+        a = [10, 22, 31, 4, 15, 28, 17, 88, 59]
+        l = LinearProbing()
+        q = QuadraticProbing()
+        d = DoubleHashing()
+        for v in a:
+            l.insert(v)
+            q.insert(v)
+            d.insert(v)
+        self.assertEqual(l.slots, [22, 88, None, None, 4, 15, 28, 17, 59, 31, 10])
+        self.assertEqual(q.slots, [22, None, 88, 17, 4, None, 28, 59, 15, 31, 10])
+        self.assertEqual(d.slots, [22, None, 59, 17, 4, 15, 28, 88, None, 31, 10])
+
+
+if __name__ == '__main__':
+    unittest.main()
