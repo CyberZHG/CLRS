21de41affaf6aaeb93d449f22512e81cf223f60f
Zhao HG <cyberzhg@gmail.com>
2016-03-17 15:20:01 +0800

    Update Chapter_09_Medians_and_Order_Statistics/9-2-b.py

--- Chapter_09_Medians_and_Order_Statistics/9-2-b.py
+++ Chapter_09_Medians_and_Order_Statistics/9-2-b.py
@@ -5,7 +5,7 @@ import unittest
 def weighted_median(x):
     x.sort()
     s = 0.0
-    for i in range(len(x)):
+    for i in xrange(len(x)):
         s += x[i]
         if s >= 0.5:
             return x[i]
@@ -14,8 +14,8 @@ def weighted_median(x):
 class ProblemTestCase(unittest.TestCase):
 
     def test_random(self):
-        for _ in range(1000):
-            a = random.sample(range(10000), 1000)
+        for _ in xrange(1000):
+            a = random.sample(xrange(10000), 1000)
             s = sum(a)
             x = [float(v) / s for v in a]
             w = weighted_median(x)
@@ -25,8 +25,8 @@ class ProblemTestCase(unittest.TestCase):
                     s1 += v
                 elif v > w:
                     s2 += v
-            self.assertTrue(s1 < 0.5 - 1e-8)
-            self.assertTrue(s2 <= 0.5 - 1e-8)
+            self.assertTrue(s1 < 0.5 + 1e-8)
+            self.assertTrue(s2 <= 0.5 + 1e-8)
 
 
 if __name__ == '__main__':
