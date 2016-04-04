a6a2565902d8d13703adc33a47b04c297b0bd429
Zhao HG <cyberzhg@gmail.com>
2016-04-04 19:45:01 +0800

    Add files

---
+++ Chapter_32_String_Matching/32.3-2.py
@@ -0,0 +1,39 @@
+import unittest
+
+
+def compute_transition_function(p, s):
+    m = len(p)
+    d = {}
+    for q in range(m + 1):
+        for a in s:
+            k = min(m + 1, q + 2)
+            while k > 0:
+                k -= 1
+                if p[:k] == (p[:q] + a)[-k:]:
+                    break
+            d[(q, a)] = k
+    return d
+
+
+class ProblemTestCase(unittest.TestCase):
+
+    def print_sorted(self, x, s):
+        x = sorted(list(map(lambda (k, v): (k, v), x.items())))
+        l = len(s)
+        output = ''
+        for i in xrange(len(x)):
+            if i % l == 0:
+                print(output)
+                output = str(i / l)
+            output += ' ' + str(x[i][1])
+        print(output)
+
+    def test_case(self):
+        s = 'ab'
+        self.print_sorted(compute_transition_function('aabab', s), s)
+        self.print_sorted(compute_transition_function('ababbabbababbababbabb',
+                                                      s), s)
+
+
+if __name__ == '__main__':
+    unittest.main()
