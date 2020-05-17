7a295f678142c0210df3128e688669d2d0592e13
CyberZHG <853842+CyberZHG@users.noreply.github.com>
2020-05-17 15:45:49 +0800

    Decrease the number of random tests

--- Chapter_13_Red-Black_Trees/13-4.py
+++ Chapter_13_Red-Black_Trees/13-4.py
@@ -121,7 +121,7 @@ class ProblemTestCase(unittest.TestCase):
         self.assertEqual(T.to_list(), [8, 12, 19, 31, 38, 41])
 
     def test_random(self):
-        for _ in range(1000):
+        for _ in range(100):
             T = Treap()
             a = random.sample(range(100000000), 1000)
             for v in a:
